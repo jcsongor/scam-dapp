bea61597919c5cf0dcbdc8b0092ed27e23267cab
Jozsa Csongor <jozsa.csongor@gmail.com>
2021-05-26 16:17:08 +0200

    fix decimals

--- src/lib/web3.js
+++ src/lib/web3.js
@@ -2,11 +2,13 @@ import { BscConnector } from "@binance-chain/bsc-connector";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { scamToken } from "../config/contracts";
 
-const getContract = (contract, account, web3) => new web3.eth.Contract(contract.abi, contract.address, {from: account});
+const getContract = (contract, account, web3) => new web3.eth.Contract(contract.abi, contract.address, { from: account });
 
-export const balanceOf = async (web3, account) =>  {
+export const balanceOf = async (web3, account) => {
 	const contract = getContract(scamToken, account, web3);
-	return await contract.methods.balanceOf(account).call();
+	const balance = await contract.methods.balanceOf(account).call();
+	const decimals = await contract.methods.decimals().call();
+	return balance / 10 ** decimals;
 };
 
 const supportedChainIds = [56, 97];
