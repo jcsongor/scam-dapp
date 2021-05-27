edfae678441fa402370441c16abf6f2b0253ecfb
Jozsa Csongor <jozsa.csongor@gmail.com>
2021-05-27 15:49:46 +0200

    make logout dialog look nicer

--- src/components/Wallet/Connected.jsx
+++ src/components/Wallet/Connected.jsx
@@ -1,12 +1,33 @@
 import React, { useMemo, useState } from "react";
-import { Button, Dialog } from '@material-ui/core/';
+import {
+	Button,
+	Dialog,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	IconButton,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	makeStyles,
+} from '@material-ui/core/';
 import { useWeb3 } from "../../hooks/useWeb3";
 import { maskAddress } from "../../lib/web3";
 import { WalletButton } from "./WalletButton";
+import CloseIcon from '@material-ui/icons/Close';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-const DisconnectButton = ({ onClick }) => <Button type="button" onClick={onClick}>Logout</Button>;
+const useStyles = makeStyles({
+	closeButton: {
+		position: 'absolute',
+		right: 0,
+		top: 7,
+	},
+});
 
 export const Connected = () => {
+	const styles = useStyles();
 	const [open, setOpen] = useState(false);
 	const { account, deactivate } = useWeb3();
 	const openDialog = useMemo(() => () => setOpen(true), [setOpen]);
@@ -15,7 +36,22 @@ export const Connected = () => {
 	return <>
 		<WalletButton onClick={openDialog}>{maskAddress(account)}</WalletButton>
 		<Dialog open={open} onClose={closeDialog}>
-			<DisconnectButton onClick={deactivate} title="Connect with Binance Chain Wallet" />
+			<DialogTitle>Logout
+				<IconButton aria-label="close" className={styles.closeButton} onClick={closeDialog}>
+					<CloseIcon />
+				</IconButton>
+			</DialogTitle>
+			<DialogContent>
+				<DialogContentText>
+					<List>
+						<ListItem button onClick={deactivate}>
+							<ListItemIcon><ExitToAppIcon /></ListItemIcon>
+							<ListItemText>Disconnect Your Wallet</ListItemText>
+						</ListItem>
+					</List>
+				</DialogContentText>
+			</DialogContent>
 		</Dialog>
-	</>;
+	</>
+		;
 };
