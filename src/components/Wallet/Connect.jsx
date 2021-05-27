c5ad431d3e2f1dbc225749adbc5dc61df9749aac
Jozsa Csongor <jozsa.csongor@gmail.com>
2021-05-27 15:40:14 +0200

    make connect dialog look nicer

--- src/components/Wallet/Connect.jsx
+++ src/components/Wallet/Connect.jsx
@@ -1,21 +1,59 @@
 import React, { useMemo, useState } from "react";
-import { Button, Dialog } from '@material-ui/core/';
+import {
+	Dialog,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+	IconButton,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	makeStyles,
+} from '@material-ui/core/';
+import CloseIcon from '@material-ui/icons/Close';
 import { useConnectWallet } from "../../hooks/useConnectWallet";
 import { WalletButton } from "./WalletButton";
 
-const ConnectButton = ({ onClick, title = 'Connect' }) => <Button onClick={onClick}>{title}</Button>;
+const useStyles = makeStyles({
+	closeButton: {
+		position: 'absolute',
+		right: 0,
+		top: 7,
+	},
+});
+
+const ConnectButton = ({ onClick, title = 'Connect', logo }) => <ListItem button onClick={onClick}>
+	<ListItemIcon><img src={logo} alt={title} width={32} height={32} /></ListItemIcon>
+	<ListItemText>{title}</ListItemText>
+</ListItem>;
 
 export const Connect = () => {
+	const styles = useStyles();
 	const [open, setOpen] = useState(false);
 	const { connectBinanceWallet, connectMetamask } = useConnectWallet();
 	const openDialog = useMemo(() => () => setOpen(true), [setOpen]);
 	const closeDialog = useMemo(() => () => setOpen(false), [setOpen]);
+	const wallets = [
+		{ title: "Metamask", logo: "/metamask-logo.svg", onClick: connectMetamask },
+		{ title: "Binance Chain Wallet", logo: "/binance-logo.png", onClick: connectBinanceWallet },
+	];
 
 	return <>
 		<WalletButton onClick={openDialog}>Connect</WalletButton>
 		<Dialog open={open} onClose={closeDialog}>
-			<ConnectButton onClick={connectBinanceWallet} title="Connect with Binance Chain Wallet" />
-			<ConnectButton onClick={connectMetamask} title="Connect with Metamask" />
+			<DialogTitle>Connect to a Wallet
+				<IconButton aria-label="close" className={styles.closeButton} onClick={closeDialog}>
+					<CloseIcon />
+				</IconButton>
+			</DialogTitle>
+			<DialogContent>
+				<DialogContentText>
+					<List>
+						{wallets.map((wallet, idx) => <ConnectButton {...wallet} key={idx} />)}
+					</List>
+				</DialogContentText>
+			</DialogContent>
 		</Dialog>
 	</>;
 };
