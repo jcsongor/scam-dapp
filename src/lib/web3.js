e3931fc097c5dd81c1e4c43dbc4e0d478d5c446e
Jozsa Csongor <jozsa.csongor@gmail.com>
2021-05-28 22:25:49 +0200

    fix rounding errors

--- src/lib/web3.js
+++ src/lib/web3.js
@@ -1,21 +1,28 @@
 import { BscConnector } from "@binance-chain/bsc-connector";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { lpMonitor, scamToken } from "../config/contracts";
+import { Decimal } from "decimal.js";
 
 const getContract = (contract, account, web3) => new web3.eth.Contract(contract.abi, contract.address, { from: account });
 
+const numberToString = (number, decimals) => {
+	const decimal = new Decimal(number);
+	return decimal.dividedBy(new Decimal(10).toPower(decimals)).toString();
+};
+
 export const balanceOf = async (web3, account) => {
 	const contract = getContract(scamToken, account, web3);
-	const balance = await contract.methods.balanceOf(account).call();
-	const decimals = await contract.methods.decimals().call();
-	return balance / 10 ** decimals;
+	const balance = new Decimal(await contract.methods.balanceOf(account).call());
+	const decimals = parseInt(await contract.methods.decimals().call());
+	return numberToString(balance, decimals);
 };
 
 const LP_DECIMALS = 18;
 
 export const lpBalanceOf = async (web3, account) => {
 	const contract = getContract(lpMonitor, account, web3);
-	return await contract.methods.balanceOf(account).call() / 10 ** LP_DECIMALS;
+	const balance = await contract.methods.balanceOf(account).call();
+	return numberToString(balance, LP_DECIMALS);
 };
 
 export const isAddressRegistered = async (web3, account) => {
